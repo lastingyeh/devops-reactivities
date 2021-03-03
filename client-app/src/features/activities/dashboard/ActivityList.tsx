2a4918b86c64035db902e6acf518507e8d789c17
Yuhung Yeh <yuhung.yeh@advantech.com.tw>
2021-03-03 11:38:17 +0800

    +[client-app] for crud of activity

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/layout/models/activity';
+
+interface Props {
+	activities: Activity[];
+	selectActivity: (id: string) => void;
+	deleteActivity: (id: string) => void;
+}
+
+export default function ActivityList({
+	activities,
+	selectActivity,
+	deleteActivity,
+}: Props) {
+	return (
+		<Segment>
+			<Item.Group divided>
+				{activities.map(activity => (
+					<Item key={activity.id}>
+						<Item.Content>
+							<Item.Header as='a'>{activity.title}</Item.Header>
+							<Item.Meta>{activity.date}</Item.Meta>
+							<Item.Description>
+								<div>{activity.description}</div>
+								<div>
+									{activity.city}, {activity.venue}
+								</div>
+							</Item.Description>
+							<Item.Extra>
+								<Button
+									floated='right'
+									content='View'
+									color='blue'
+									onClick={() => selectActivity(activity.id)}
+								/>
+								<Button
+									floated='right'
+									content='Delete'
+									color='red'
+									onClick={() => deleteActivity(activity.id)}
+								/>
+								<Label basic content={activity.category} />
+							</Item.Extra>
+						</Item.Content>
+					</Item>
+				))}
+			</Item.Group>
+		</Segment>
+	);
+}
