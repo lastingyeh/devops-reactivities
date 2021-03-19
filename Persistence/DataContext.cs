50d2ff200a379aec4113d39b26f50171eb73f236
Yuhung Yeh <yuhung.yeh@advantech.com.tw>
2021-03-19 10:19:49 +0800

    [api] Photos

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,6 +9,7 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options) { }
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
